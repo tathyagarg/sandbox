00f238c0663597fcd1bf20e10ebf18e1725edff4
Tathya Garg <tathya.garg@gmail.com>
2024-02-20 02:19:20 +0530

    Added concrete being able to stop sand from falling

--- constants.py
+++ constants.py
@@ -1,8 +1,9 @@
 WIDTH = 1000
 HEIGHT = 1000
 RENDER_SIZE = 16  # The size of the rendered pixel
-GRAVITY = 10
+GRAVITY = 2.5
 
 
 class COLORS:
     CONCRETE = (200, 200, 200)
+    SAND = (224, 183, 135)
